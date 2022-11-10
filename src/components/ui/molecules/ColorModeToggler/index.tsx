2fb7aa2b01f154aaf5fa940c4aab5885dec87983
Francy Helder Silva de Almeida <francyalmeida@ufgd.edu.br>
2022-11-10 17:31:38 -0400

    Change ColorModeToggler component

--- src/components/ui/molecules/ColorModeToggler/index.tsx
+++ src/components/ui/molecules/ColorModeToggler/index.tsx
@@ -1,12 +1,33 @@
-import { useColorMode, Icon } from '@chakra-ui/react'
+import { useColorMode, Icon, Switch, HStack } from '@chakra-ui/react'
 import { Button } from '@chakra-ui/button';
 import { RiSunFill, RiMoonLine } from "react-icons/ri";
 
 const ColorModeToggler = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode, setColorMode } = useColorMode()
+  const isDark = colorMode === "dark"
+
+
  
   return (
-    <Button maxW={50} onClick={toggleColorMode}><Icon as={ colorMode === "light"  ? RiMoonLine : RiSunFill } /></Button>
+    <HStack justify="center">
+      <Icon 
+        onClick={() => setColorMode("light")} 
+        as={RiSunFill} 
+        color={ isDark ? "gray.400" : undefined}
+      />
+      <Switch 
+        colorScheme='gray' 
+        size='lg'
+        defaultChecked={isDark}
+        isChecked={isDark}
+        onChange={toggleColorMode} 
+      />
+      <Icon 
+        onClick={() => setColorMode("dark")} 
+        as={RiMoonLine} 
+        color={ !isDark ? "gray.300" : undefined}
+      />
+    </HStack>
   )
 }
 
