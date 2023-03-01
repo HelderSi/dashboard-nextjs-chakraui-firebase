9449a2ab61675a811434e57add08187058f4fdfe
Francy Helder Silva de Almeida <francyalmeida@ufgd.edu.br>
2023-03-01 12:49:18 -0400

    wip: google social login

--- src/services/firebase/auth.ts
+++ src/services/firebase/auth.ts
@@ -12,17 +12,46 @@ import {
     UserProfile,
     AuthCredential,
     User,
-    EmailAuthProvider
+    EmailAuthProvider,
+    GoogleAuthProvider,
+    signInWithRedirect,
+    getRedirectResult
 } from 'firebase/auth';
 import init from './init'
 
 init()
 const auth = getAuth();
+auth.useDeviceLanguage();
+const googleAuthProvider = new GoogleAuthProvider();
 
 export default {
     getAuth: () => auth,
     getCurrentUser: () => auth.currentUser,
     signInWithEmailAndPassword: (email: string, password: string) => signInWithEmailAndPassword(auth, email, password),
+
+    signInWithGoogle: () => signInWithRedirect(auth, googleAuthProvider),
+
+    getRedirectResult: () => getRedirectResult(auth)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access Google APIs.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        }),
+
     createUserWithEmailAndPassword: (email: string, password: string) => createUserWithEmailAndPassword(auth, email, password),
     onAuthStateChanged,
     sendPasswordResetEmail: (email: string) => sendPasswordResetEmail(auth, email),
